d992d2ed4cc81416b900eb75151da82598566230
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-11-03 11:36:10 -0300

    Exercicio 3.1

---
+++ Capitulo 3 - Variáveis e entradas de dados/exercicios_capitulo3/exercicio3_1.py
@@ -0,0 +1,30 @@
+# Exercício 3.1 - Complete a tabela a seguir, marcando inteiro ou ponto flutuante dependendo do número apresentado
+
+# como o exercicio no livro trata-se de uma tabela irei atribuir os valores a variavies e imprimir os tipos de cada um
+
+a = 5
+# 5 é um valor do tipo inteiro
+
+b = 50
+# 50 é um valor do tipo inteiro
+
+c = 4.3
+# 4.3 é um valor do tipo ponto flutuante
+
+d = -2
+# -2 é um valor do tipo inteiro
+
+e = 100
+# 100 é um valor do tipo inteiro
+
+f = 1.333
+# 1.333 é um valor do tipo ponto flutuante
+
+# imprimindo os resultados
+print(a, 'é um valor do tipo:', type(a))
+print(b, 'é um valor do tipo:', type(b))
+print(c, 'é um valor do tipo:', type(c))
+print(d, 'é um valor do tipo:', type(d))
+print(e, 'é um valor do tipo:', type(e))
+print(f, 'é um valor do tipo:', type(f))
+
