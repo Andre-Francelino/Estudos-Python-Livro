a9eeeb9c77c729abf38655bb177da4be59298285
Andre Francelino <andrefgomes.github.version@gmail.com>
2025-02-19 21:08:42 -0300

    representaç~ão de valores numÂéricos

---
+++ Capitulo 3 - Variáveis e entradas de dados/programas_capitulo3/prog_representacao_val_numericos.py
@@ -0,0 +1,15 @@
+# testando calculo mencionado na pág. 54
+teste_valores_numericos = 3 * 1.0
+print(teste_valores_numericos)
+
+# Base 2 - binário (0b)
+a = 0b10
+print('A base númerica da operação binária é:', a)
+
+# Base 16 - hexadecimal (0x)
+b = 0x10
+print('A base númerica da operação hexadecimal é:', b)
+
+# Base 8 - octal (0o)
+c = 0o10
+print('A base númerica da operação octal é:', c)
